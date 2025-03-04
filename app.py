c90e7cd5a4758ec40c8cbe43d98b47486c5a3c9e
DannaMAC <116323415+DannaMAC@users.noreply.github.com>
2025-03-03 20:41:50 -0600

    ultima modificación

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ st.write("Ingrese las características de la casa para predecir su precio.")
 
 longitude = st.number_input("Longitud", value=122.23)
 latitude = st.number_input("Latitud", value=37.88)
-housing_median_age = st.number_input("Edad de la Vivienda", value=41.0)
-total_rooms = st.number_input("Número Total de Habitaciones", value=8.0)
-total_bedrooms = st.number_input("Número Total de Dormitorios", value=12.0)
-population = st.number_input("Población", value=32.0)
-households = st.number_input("Número de Hogares", value=120.0)
-median_income = st.number_input("Ingreso Mediano", value=8.3252)
+housing_median_age = st.number_input("Edad de la Vivienda", value=41)
+total_rooms = st.number_input("Número Total de Habitaciones", value=12)
+total_bedrooms = st.number_input("Número Total de Dormitorios", value=12)
+population = st.number_input("Población", value=32)
+households = st.number_input("Número de Hogares", value=120)
+median_income = st.number_input("Ingreso Mediano", value=8.32)
 
 if st.button("Predecir Precio"):
     input_data = np.array([[longitude, latitude, housing_median_age, total_rooms, total_bedrooms, 
